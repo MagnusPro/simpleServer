a5e0c88549e4c8c5a4423e6a696605ab1018ba23
MagnusPro <volodymyrskorik@gmail.com>
2017-04-03 19:59:08 +0300

    something working

--- main.py
+++ main.py
@@ -2,69 +2,75 @@ import socket
 import os
 import mimetypes
 from contextlib import closing
+from sys import argv
 
 class server:
     def __init__(self, port=8000):
         self.port = port
 
     def run(self):
+        """
+        Start server
+        """
         with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
             sock.bind(("localhost", self.port))
             sock.listen(10)
             while 1:
                 conn, addr = sock.accept()
-                print("New connection from ", addr)
                 try:
                     self._handle_client(conn)
                 except Exception as e:
                     print(e)
-                    self._http_response(conn, "500 Internal Server Error", data=b"Server error")
+                    self._send_error(conn, "500 Internal Server Error", message="Server error")
                 finally:
                     conn.close()
 
     def _handle_client(self,conn):
-
+        """
+        Handle one client request
+        """
         request = self._get_request(conn)
         if request is not None:
             request_header = request.split("\r\n", 1)[0]
             method, address, protocol = request_header.split(" ", 2)
 
             if method != "GET":
-                self._http_response(conn, "404 Not Found", data=b"I cant handle Post request")
+                self._send_error(conn, "404 Not Found", message="I cant handle Post request")
                 return
             else:
                 self._get_method(conn, address)
 
     def _get_method(self, conn, address):
-        send_type = "text/html; charset=utf-8"
+        """
+        Handle GET request
+        """
         address = "." + address
         if os.path.isdir(address):
             index = os.path.join(address, "index.html")
             if os.path.isfile(index):
-                answer, send_type = self._read_file(index) #(prepare_page(index)).encode("utf-8")
+                self._send_file(conn, index)
             else:
-                answer = (self._show_dir(address)).encode("utf-8")
+                self._send_dir(conn, address)
         elif os.path.isfile(address):
-            answer, send_type = self._read_file(address)
+            self._send_file(conn, address)
         else:
-            self._http_response(conn, "404 Not Found", data=b"I cant handle Post request")
-            return
+            self._send_error(conn, "404 Not Found", message="I cant handle Post request")
 
-        self._http_response(conn, typ=send_type, data=answer)
-
-    def _http_response(self, conn, status="200 OK", typ="text/plain; charset=utf-8", data=b""):
-        # строка состояния
+    def _http_header(self, conn, length, status="200 OK", typ="text/plain; charset=utf-8"):
+        """
+        Send request line and headers
+        """
         conn.send(b"HTTP/1.1 " + status.encode("utf-8") + b"\r\n")
-        # заголовки
         conn.send(b"Server: simplehttp\r\n")
-        #conn.send(b"Connection: keep-alive\r\n")
+        conn.send(b"Connection: close\r\n")
         conn.send(b"Content-Type: " + typ.encode("utf-8") + b"\r\n")
-        conn.send(b"Content-Length: " + str(len(data)).encode("utf-8") + b"\r\n")
+        conn.send(b"Content-Length: " + str(length).encode("utf-8") + b"\r\n")
         conn.send(b"\r\n")
-        # тело
-        conn.send(data)
 
     def _get_request(self, conn):
+        """
+        Get client request
+        """
         data = b""
         while not b"\r\n" in data:
             tmp = conn.recv(1024)
@@ -77,27 +83,50 @@ class server:
             return
         return data.decode("utf-8")
 
-    def _read_file(self, address):
+    def _send_file(self, conn, address):
+        """
+        Send file to client
+        """
         send_type, encoding = mimetypes.guess_type(address)
-        print(encoding)
         if send_type is None:
             send_type = "application/octet-stream"
+        self._http_header(conn, os.path.getsize(address), typ=send_type)
         with open(address, "rb") as file:
-            data = file.read()
-        return data, send_type
+            data = file.read(1024)
+            while data:
+                conn.send(data)
+                data = file.read(1024)
 
-    def _show_dir(self, address):
+    def _send_dir(self, conn, address):
+        """
+        Send dir structure to client
+        """
         data = "<!DOCTYPE html>"
         data += "<html><head><title>Dir</title></head><body><h1>" + address[1:] + "</h1><ul>"
-        for name in os.listdir(address):
+        for name in sorted(os.listdir(address)):
             link = os.path.join(address, name)
             if os.path.isdir(link):
                 link += "/"
                 name += "/"
             data += "<li><a href=" + link[1:] + ">" + name + "</a></li>"
         data += "</ul></body></html>"
-        return data
+        data = data.encode("utf-8")
+        send_type = "text/html; charset=utf-8"
+        self._http_header(conn, length=len(data), typ=send_type)
+        conn.send(data)
+
+    def _send_error(self, conn, status, message):
+        """
+        Send error to client
+        """
+        self._http_header(conn, len(message.encode("utf-8")), status)
+        conn.send(message.encode("utf-8"))
 
 if __name__ == "__main__":
-    s = server()
+    serv_port = 8000
+    if len(argv) == 2:
+        port = int(argv[1])
+        if port in range(1, 65535):
+            serv_port = port
+    s = server(serv_port)
     s.run()
